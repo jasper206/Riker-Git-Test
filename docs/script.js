03995bb35ad4dc6fceab2d5a1646b76c3af333af
Riker <jaspa206@gmail.com>
2024-10-11 10:54:54 +0100

    BG change on click via id

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,7 @@
 const toggleMenu = () => {
   document.querySelector(".leftPanel").classList.toggle("open");
 };
+const goRed = (clicked_id) => {
+  document.querySelector(`#${clicked_id}`).classList.toggle("red");
+  console.log(`#${clicked_id}`);
+};
